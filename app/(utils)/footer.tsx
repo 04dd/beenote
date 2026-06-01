5bcf79b9cf1a3fec729a9a5534dbdd3e164efb7e
Fei Li <fei.li.best@outlook.com>
2026-06-01 13:05:56 +0800

    update github url

--- app/(utils)/footer.tsx
+++ app/(utils)/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="footer footer-center p-10 bg-primary text-primary-content">
       <div>
         <Logo size={50}/>
-        <p className="font-bold">Built by <a href="https://github.com/volagold/beenote" target="_blank" rel="noreferrer"><u>volagold</u></a></p> 
+        <p className="font-bold">Built by <a href="https://github.com/04dd/beenote" target="_blank" rel="noreferrer"><u>04dd</u></a></p> 
         <p>A Next.js Project</p>
         <p>Copyright © 2025 - All rights reserved</p>
       </div> 
@@ -20,7 +20,7 @@ export default function Footer() {
           <a title="X (twitter)" href="https://x.com" target="_blank" rel="noreferrer">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="23" height="23" className="fill-current"><path d="M8 2H1L9.26086 13.0145L1.44995 21.9999H4.09998L10.4883 14.651L16 22H23L14.3917 10.5223L21.8001 2H19.1501L13.1643 8.88578L8 2ZM17 20L5 4H7L19 20H17Z"></path></svg>
           </a>
-          <a title="GitHub Repo" href="https://github.com/volagold/beenote" target="_blank" rel="noreferrer">
+          <a title="GitHub Repo" href="https://github.com/04dd/beenote" target="_blank" rel="noreferrer">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="fill-current"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z"/></svg>
           </a>
         </div>
